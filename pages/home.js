1f59406e6ba21a5cd5a28532c3a4771778b36e77
Matthew <ricklefsmatthew1003@gmail.com>
2022-05-14 20:07:14 -0400

    integrates custom class within our components layer in global.css, beings building landing page

--- pages/home.js
+++ pages/home.js
@@ -1,4 +1,5 @@
 import {
+  ArrowForwardIosRounded,
   BusinessCenter,
   ExploreOutlined,
   GroupOutlined,
@@ -30,12 +31,37 @@ function Home() {
           </div>
 
           <div className="pl-4">
-            <button className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2">
+            <button className="text-blue-700 hover:text-blue-400 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-blue-400">
               Sign In
             </button>
           </div>
         </div>
       </header>
+
+      <main className="flex flex-col xl:flex-row items-center max-w-screen-lg mx-auto">
+        <div className="space-y-6 xl:space-y-10">
+          <h1 className="text-3xl md:text-5xl text-amber-800/80 max-w-xl !leading-snug pl-4 xl:pl-0 font-light">
+            Welcome to your professional community.
+          </h1>
+
+          <div className="space-y-4">
+            <div className="intent">
+              <h2 className="text-xl">Search for a job</h2>
+              <ArrowForwardIosRounded className="text-gray-700" />
+            </div>
+
+            <div className="intent">
+              <h2 className="text-xl">Find a person that you know</h2>
+              <ArrowForwardIosRounded className="text-gray-700" />
+            </div>
+
+            <div className="intent">
+              <h2 className="text-xl">Learn a new skill</h2>
+              <ArrowForwardIosRounded className="text-gray-700" />
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
